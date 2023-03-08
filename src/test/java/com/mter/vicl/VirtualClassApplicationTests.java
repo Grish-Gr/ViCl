468a84ef440555bb2d754491dd8bfa812f26ca00
Grish-Gr <101037462+Grish-Gr@users.noreply.github.com>
2023-03-08 12:38:11 +0500

    start project

---
+++ src/test/java/com/mter/vicl/VirtualClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mter.vicl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
